da5bfbac8174f3c7a5f6905811804a2af0acec7c
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-11-01 05:33:12 +0100

    email host user config

--- hallmarts/settings.py
+++ hallmarts/settings.py
@@ -191,8 +191,8 @@ WSGI_APPLICATION = 'hallmarts.wsgi.application'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ekonge903@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD' ) #past the key or password app here
+EMAIL_HOST_USER =config('EMAIL_HOST_USER' ) 
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD' ) #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
